fd20ddac342214d74173db9b0295be40cf051ab4
praeto89 <Praetosc2@gmail.com>
2026-03-16 07:19:19 +0100

    Ok

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var audio := $AudioStreamPlayer2D
+
 func _ready() -> void:
 	$AnimatedSprite2D.play("spin")
 	body_entered.connect(_on_body_entered)
@@ -7,4 +9,6 @@ func _ready() -> void:
 func _on_body_entered(body: Node) -> void:
 	if body is CharacterBody2D:
 		body.collect_coin()
+		audio.play()
+		await get_tree().create_timer(audio.stream.get_length()).timeout
 		queue_free()
