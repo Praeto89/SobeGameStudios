819b1cfeb3e92dbe4e3d7c6989751bca888c3a7e
praeto89 <Praetosc2@gmail.com>
2026-04-29 12:53:38 +0200

    Moritz_Räume

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,6 +1,7 @@
-extends CharacterBody2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+class_name Player extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -300.0
 const ROLL_SPEED = 300.0
 const CHARGE_SPEED = 700.0
 const CHARGE_DURATION = 0.5
