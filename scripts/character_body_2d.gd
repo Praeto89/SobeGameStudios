11de706cdb592211cfe4219a6dfe79fe088625de
praeto89 <Praetosc2@gmail.com>
2026-05-12 15:38:13 +0200

    spielgefühl

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -26,9 +26,9 @@ class_name Player extends CharacterBody2D
 # Bewegungs-Konstanten
 # -----------------------------------------------------------------------------
 const SPEED = 150.0                 # Horizontale Laufgeschwindigkeit am Boden
-const AIR_SPEED = 180.0             # Horizontale Geschwindigkeit in der Luft (leicht reduziert)
+const AIR_SPEED = 100.0             # Horizontale Geschwindigkeit in der Luft (leicht reduziert)
 const JUMP_VELOCITY = -400.0        # Initialer Aufwaertsimpuls beim Springen (negativ = nach oben)
-const JUMP_RELEASE_MULTIPLIER = 2.5 # Gravity-Multiplikator wenn Sprung losgelassen wird (variable Hoehe)
+const JUMP_RELEASE_MULTIPLIER  = 2.5 # Gravity-Multiplikator wenn Sprung losgelassen wird (variable Hoehe)
 const ROLL_SPEED = 300.0            # Geschwindigkeit waehrend des Rolls
 const CHARGE_SPEED = 700.0          # Geschwindigkeit waehrend des Charge-Dashs
 const CHARGE_DURATION = 0.5         # Sekunden die "charge" gehalten werden muss um auszuloesen
--- scripts/charge_ability.gd
+++ scripts/charge_ability.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
-func _ready() -> void:
+func _ready():
 	body_entered.connect(_on_body_entered)
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.unlock_wallcrawl()
 		queue_free()
